12472fef9df1943407344e7ddb18c00f135120ec
mrhammad04x <jagralahammadimran00@gmail.com>
2026-02-03 17:53:32 +0530

    INITIAL COMMIT

--- src/app/(routes)/page.js
+++ src/app/(routes)/page.js
@@ -1,30 +1,25 @@
 
 import HeroSection from "@/components/home/HeroSection";
-import SliderSection from "@/components/home/AlsoLikeSection";
 import Navbar from "@/components/layout/Navbar";
 import AlsoLikeSection from "@/components/home/AlsoLikeSection";
 import PopularDestinations from "@/components/home/PopularDestinations";
-import SliderSection from "@/components/home/SliderSection";
 import Footer from "@/components/layout/Footer";
 
 import AdventureTypeSlider from "@/components/home/AdventureTypeSlider";
 import TopPackages from "@/components/home/TopPackages";
 
 export default function Home() {
-  return (
-    <>
-      <Navbar/>
-      <div className="mt-32">
-        <HeroSection />
-      </div>
-      <PopularDestinations/>
-      <AlsoLikeSection/>
-      {/* <HeroSection />
-      <SliderSection /> */}
-      <Footer/>
-    </>
-      <AdventureTypeSlider />
-      <TopPackages />
+    return (
+        <>
+            <Navbar />
+            <div className="mt-32">
+                <HeroSection />
+            </div>
+            <AdventureTypeSlider />
+            <TopPackages />
+            <PopularDestinations />
+            <AlsoLikeSection />
+            <Footer />
         </>
-  );
+    );
 }
